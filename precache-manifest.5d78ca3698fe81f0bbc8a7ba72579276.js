0f7fb8f0394f63772ff513cbd2e1edd8e988e0dd
Venus Huang <venus.huang@pwc.com>
2019-08-05 18:04:10 +0800

    Deploy react sudoku demo

---
+++ precache-manifest.5d78ca3698fe81f0bbc8a7ba72579276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c129dee4198ecc85c4ec4ab369735493",
+    "url": "/react-sudoku/index.html"
+  },
+  {
+    "revision": "fceebf95cb9ab7bc7c17",
+    "url": "/react-sudoku/static/css/main.58ddf5ed.chunk.css"
+  },
+  {
+    "revision": "cc6ff193adc43f752aa6",
+    "url": "/react-sudoku/static/js/2.7435d3eb.chunk.js"
+  },
+  {
+    "revision": "fceebf95cb9ab7bc7c17",
+    "url": "/react-sudoku/static/js/main.74e8bd45.chunk.js"
+  },
+  {
+    "revision": "c0d938cc65d9ca8b6d77",
+    "url": "/react-sudoku/static/js/runtime~main.a65cae2d.js"
+  }
+]);
