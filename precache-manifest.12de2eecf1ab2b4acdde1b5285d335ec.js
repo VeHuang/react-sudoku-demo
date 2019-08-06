aff02a04ee99680838e9d006f466165cd0232234
VeHuang <726631412@qq.com>
2019-08-06 11:40:17 +0800

    change file url

---
+++ precache-manifest.12de2eecf1ab2b4acdde1b5285d335ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fdac05d76dca5367c671a271b7e3fb",
+    "url": "/react-sudoku-demo/index.html"
+  },
+  {
+    "revision": "fceebf95cb9ab7bc7c17",
+    "url": "/react-sudoku-demo/static/css/main.58ddf5ed.chunk.css"
+  },
+  {
+    "revision": "cc6ff193adc43f752aa6",
+    "url": "/react-sudoku-demo/static/js/2.7435d3eb.chunk.js"
+  },
+  {
+    "revision": "fceebf95cb9ab7bc7c17",
+    "url": "/react-sudoku-demo/static/js/main.74e8bd45.chunk.js"
+  },
+  {
+    "revision": "6502dc88e5a454314b97",
+    "url": "/react-sudoku-demo/static/js/runtime~main.ba8dab85.js"
+  }
+]);
